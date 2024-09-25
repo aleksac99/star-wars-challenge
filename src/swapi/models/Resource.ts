af92e1cb9a3b7c19de66017f1dc5c9fbfc11deac
aleksac99 <aleksa.cvetanovic99@gmail.com>
2024-09-25 07:21:29 +0200

    Add resource object caching

--- src/swapi/models/Resource.ts
+++ src/swapi/models/Resource.ts
@@ -2,17 +2,21 @@ import axios from "axios";
 import { Person } from "./Person";
 
 export abstract class Resource {
-    name: string;
-    readonly type: string;
     static resource: string;
+    static cache: Map<string, object> = new Map();
+    name: string;
     people: string[];
+    readonly type: string;
 
     static async fetchPeople(urls: string[]) {
 
         const people =  await Promise.all(
             urls.map(async (url: string) => {
-                const response = await axios.get<Person>(url);
-                return Person.fromObject(response.data);}));
+                if (!Resource.cache.has(url)) {
+                    const response = await axios.get<Person>(url);
+                    Resource.cache.set(url, response.data);
+                }
+                return Person.fromObject(Resource.cache.get(url));}));
 
         return people;
     }
